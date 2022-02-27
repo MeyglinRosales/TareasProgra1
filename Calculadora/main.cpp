f0552adaa723dcadfd7d060e802df0ec176988a2
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-02-27 13:24:18 -0600

    Se ha agregado tarea de la calcuadora

---
+++ Calculadora/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Num1 = 0;
+    int Num2 = 0;
+    cout << "Introduce el primer numero: " << endl;
+    cin >> Num1;
+    cout << "Introduce e segundo numero" << endl;
+    cin >> Num2;
+    cout << "La suma es: " << Num1 + Num2 << endl;
+    cout << "La resta es: " << Num1 - Num2 << endl;
+    cout << "La multiplicacion es: " << Num1 * Num2 << endl;
+    cout << "La division es: " << Num1 / Num2 << endl;
+    return 0;
+
+}
