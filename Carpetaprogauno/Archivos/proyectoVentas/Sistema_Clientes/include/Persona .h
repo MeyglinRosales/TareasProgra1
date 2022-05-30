367a946869cc3e2bbdcba8973d776888dfeb7f76
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-05-29 21:11:21 -0600

    actualizacion de programas ingeniero

---
+++ Carpetaprogauno/Archivos/proyectoVentas/Sistema_Clientes/include/Persona .h
@@ -0,0 +1,16 @@
+#ifndef PERSONA _H
+#define PERSONA _H
+
+
+class Persona
+{
+    public:
+        Persona ();
+        virtual ~Persona ();
+
+    protected:
+
+    private:
+};
+
+#endif // PERSONA _H
